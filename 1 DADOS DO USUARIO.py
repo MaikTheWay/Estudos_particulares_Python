14011d35967d832ce607e8b05b5e647fecf26082
MaikTheWay <108753254+MaikTheWay@users.noreply.github.com>
2022-08-29 18:58:03 -0700

    Update 1 DADOS DO USUARIO.py

--- 1 DADOS DO USUARIO.py
+++ 1 DADOS DO USUARIO.py
@@ -1,15 +1,12 @@
 # RECEBENDO DADOS DO USUARIO
-# Termianl > Python > Dir  para acessar recursos integrados a linguagem.
-# CTRL + L -> Limpa o terminal
 
-print('Olá qual seu nome?')
-nome = input()
+nome = input('Olá qual seu nome?')
 
 idade = int(input(f'Muito prazer {nome},qual sua idade ?'))
 print(f'{nome} tem {idade} anos')
 
-
 # Fazendo Cast -> todo dado recebido via input é tipo STRING
 print(f'voce nasceu em {2022 - int(idade)}')
 
-
+# Termianl > Python > Dir  para acessar recursos integrados a linguagem.
+# CTRL + L -> Limpa o terminal
